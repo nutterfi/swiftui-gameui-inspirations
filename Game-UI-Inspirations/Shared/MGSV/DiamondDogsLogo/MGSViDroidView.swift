221e3a516c572ba5fbc17068661de313d2dfcc78
Nutter Fi <itsnutterfi@gmail.com>
2021-06-26 23:06:13 -0600

    Playing more with CIFilters - image from color

--- Game-UI-Inspirations/Shared/MGSV/DiamondDogsLogo/MGSViDroidView.swift
+++ Game-UI-Inspirations/Shared/MGSV/DiamondDogsLogo/MGSViDroidView.swift
@@ -9,15 +9,34 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
+// https://stackoverflow.com/questions/26542035/create-uiimage-with-solid-color-in-swift
+extension UIColor {
+    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { rendererContext in
+            self.setFill()
+            rendererContext.fill(CGRect(origin: .zero, size: size))
+        }
+    }
+}
+
 class MGSViDroidViewModel : ObservableObject {
   @Published var titles: [String] = ["Cats pajama party", "Perfectly soggy bread", "Waterproof slipper slaps"]
 }
 
 struct MGSViDroidView: View {
   let context = CIContext()
-  @State private var filter = CIFilter.dither()
+  
+  // source imag
+  @State private var inputImage: UIImage?
+  // result image
+  @State private var image: Image?
+  @State var colorImage = UIColor.red.image(CGSize(width: 256, height: 256))
+
+  
+  @State private var filter = CIFilter.dotScreen()
   
   @StateObject private var viewModel = MGSViDroidViewModel()
+  
     var body: some View {
       GeometryReader { proxy in
         let dim = min(proxy.size.width, proxy.size.height)
@@ -60,17 +79,45 @@ struct MGSViDroidView: View {
                     .font(.title2)
 
                 }
-                .foregroundColor(.blue)
+                .foregroundColor(.white)
               }
               .listRowBackground(Color.clear)
               .frame(height: 48)
             }
           }
           
+          // Back image
+          if let image = image {
+            image
+              .resizable()
+              .scaledToFill()
+              .frame(width: dim/2, height: dim / 2)
+          }
+          
         }
         .frame(width: proxy.size.width, height: proxy.size.height)
       }
+      .onAppear {
+        loadImages()
+        processImages()
+      }
+
     }
+  
+  func loadImages() {
+    let ciimage = CIImage(image: colorImage)
+    // load the filters with image data
+    filter.setValue(ciimage, forKey: kCIInputImageKey)
+  }
+  
+  func processImages() {
+    guard let outputImage = filter.outputImage else { return }
+    if let cgimage = context.createCGImage(outputImage, from: outputImage.extent) {
+      let uiimage = UIImage(cgImage: cgimage)
+      image = Image(uiImage: uiimage)
+    }
+    
+  }
 }
 
 struct MGSViDroidView_Previews: PreviewProvider {
