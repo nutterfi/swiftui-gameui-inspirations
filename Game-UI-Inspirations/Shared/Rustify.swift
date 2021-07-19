105fc1c8d305178daca6060aa82fda90f49dec3e
Nutter Fi <itsnutterfi@gmail.com>
2021-07-19 13:44:28 -0600

    UI bug fixes on FireflyPendant
    Move circle mask from ZStack to Rustify View
    Pendant hole drawn last
    Renamed variables

--- Game-UI-Inspirations/Shared/Rustify.swift
+++ Game-UI-Inspirations/Shared/Rustify.swift
@@ -24,14 +24,14 @@ struct Rustify: View {
       GeometryReader { proxy in
         ZStack {
           RadialGradient(gradient: Gradient(colors: [.kombatBrown, .green]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: proxy.size.width * startRadius, endRadius: proxy.size.width * endRadius)
+            .mask(
+              Image(systemName: style)
+                .resizable()
+                .scaledToFit()
+                .blur(radius: blur)
+            )
         }
         .frame(width: proxy.size.width, height: proxy.size.height)
-        .mask(
-          Image(systemName: style)
-            .resizable()
-            .scaledToFit()
-            .blur(radius: blur)
-        )
       }
     }
 }
--- Game-UI-Inspirations/Shared/TheLastOfUs/FireflyPendant.swift
+++ Game-UI-Inspirations/Shared/TheLastOfUs/FireflyPendant.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct FireflyDemo: View {
-  @State private var control: CGFloat = 0.1
+struct FireflyPendantDemo: View {
+  @State private var rustLevel: CGFloat = 0.1
 
   var body: some View {
     VStack {
-      Slider(value: $control)
-      FireflyPendant(control: control, rustStyle: RustifyStyle.one)
-      FireflyPendant(control: control, rustStyle: RustifyStyle.two)
-      FireflyPendant(control: control, rustStyle: RustifyStyle.three)
+      Slider(value: $rustLevel)
+      FireflyPendant(rustLevel: rustLevel, rustStyle: RustifyStyle.one)
+      FireflyPendant(rustLevel: rustLevel, rustStyle: RustifyStyle.two)
+      FireflyPendant(rustLevel: rustLevel, rustStyle: RustifyStyle.three)
     }
     .padding()
   }
@@ -23,7 +23,7 @@ struct FireflyDemo: View {
 }
 
 struct FireflyPendant: View {
-    var control: CGFloat = 0.1
+    var rustLevel: CGFloat = 0.1
     var rustStyle: String = RustifyStyle.one
     var body: some View {
       GeometryReader { proxy in
@@ -35,24 +35,26 @@ struct FireflyPendant: View {
             .stroke(Color.black, lineWidth: 2)
             .frame(width: dim * 0.9)
           
+          
+          FireflySymbol()
+            .overlay(
+              Rustify(blur: rustLevel * 100,                         style: rustStyle)
+                .mask(Circle())
+            )
+          
+          // pendant hole
           Circle()
             .fill(Color.black)
             .frame(width: dim * 0.07)
             .offset(x: 0, y: -dim * 0.35)
-          FireflySymbol()
         }
         .frame(width: proxy.size.width, height: proxy.size.height)
-        .overlay(
-          Rustify(blur: control * 100,                         style: rustStyle)
-            .frame(width: proxy.size.width, height: proxy.size.height)
-        )
-        .mask(Circle())
       }
     }
 }
 
 struct FireflyPendant_Previews: PreviewProvider {
     static var previews: some View {
-      FireflyDemo()
+      FireflyPendantDemo()
     }
 }
