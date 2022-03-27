6843bd8e9a8d460f276c04d6297271bc54371962
Nutter Fi <itsnutterfi@gmail.com>
2022-03-26 22:53:10 -0700

    Simplified view

--- Game-UI-Inspirations/Shared/RDR2/RDR2RevolverLoadingAnimation.swift
+++ Game-UI-Inspirations/Shared/RDR2/RDR2RevolverLoadingAnimation.swift
@@ -10,32 +10,29 @@ import SwiftUI
 struct RDR2RevolverLoadingView: View {
   var body: some View {
     GeometryReader { proxy in
-      Color.black.ignoresSafeArea()
       let dim = min(proxy.size.width, proxy.size.height)
       ZStack() {
-        RDR2ShowdownTitle(title: "Hello World")
-          .frame(width: dim * 1, height: dim * 0.25)
+        Color.black.ignoresSafeArea()
         VStack {
           Spacer()
           HStack {
             Spacer()
             RDR2RevolverLoadingAnimation()
               .foregroundColor(Color.white)
-            .frame(width: dim / 5, height: dim / 5)
+              .frame(width: dim / 5, height: dim / 5)
           }
         }
       }
       .frame(width: proxy.size.width, height: proxy.size.height)
     }
   }
-  
-  
 }
 
 struct RDR2RevolverLoadingAnimation: View {
   @State private var isAnimating = false
     var body: some View {
-      RDR2Revolver().clipShape(Circle().inset(by: 1))
+      RDR2Revolver()
+        .clipShape(Circle().inset(by: 1))
         .rotationEffect(isAnimating ? .radians(.pi / 4) : .zero)
         .onAppear {
           withAnimation(Animation.spring().delay(0.25).repeatForever(autoreverses: false)) {
